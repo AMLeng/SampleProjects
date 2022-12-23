30bd84f34ef3dc0fde3596211d6fcc2b132c7c41
AMLeng <eaglelys@hotmail.com>
2022-12-23 16:09:28 -0500

    Advent of Code 2022, Day 23 Updated Solutions (Iteration 1)

--- AoC2022/23.cpp
+++ AoC2022/23.cpp
@@ -1,175 +1,189 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <bitset>
 #include <cassert>
-#include <map>
+#include <unordered_map>
 #include <utility>
 #include <climits>
 
-//Currently takes 17-18 seconds to run
-//In addition to the very poorly structured code
-//And the use of an assert to terminate the code when finished
+//Currently takes 4-5 seconds to run
 
-bool has_neighbor(const std::map<std::pair<int,int>,int>& grid, const std::pair<int,int> point){
-    assert(grid.at(point) == 0);
-    for(int i=-1; i<= 1; i++){
-        for(int j=-1; j<= 1; j++){
-            if(i == 0 && j == 0){
-                continue;
-            }
-            if(grid.find(std::make_pair(point.first + i,point.second + j)) != grid.end()){
-                return true;
-            }
+namespace std{
+    template<>
+    struct hash<std::pair<int,int>>{
+        size_t operator()(const std::pair<int,int> &p) const{
+            return static_cast<unsigned int>(p.first + 10000*p.second);
         }
-    }
-    return false;
+    };
 }
-int movement_direction(const std::map<std::pair<int,int>,int>& grid, const std::pair<int,int> point, int round){
-    assert(grid.at(point) == 0);
-    bool none = true;
-    for(int k=0; k<4; k++){
-        switch((k+round)%4){
-        case 0:
-            for(int i=-1; i<=1; i++){
-                if(grid.find(std::make_pair(point.first - 1,point.second + i))!= grid.end()){
-                    none = false;
-                    break;
+class Grid{
+    public:
+    Grid(std::vector<std::string> lines) : none_moved(false){
+        int i = 0;
+        for(auto line : lines){
+            int j=0;
+            for(char c : line){
+                if(c == '#'){
+                    grid.emplace(std::make_pair(i,j),0);
                 }
+                j++;
             }
-            if(none){
-                return -3;
-            }
-            none = true;
-            break;
-        case 1:
-            for(int i=-1; i<=1; i++){
-                if(grid.find(std::make_pair(point.first + 1,point.second + i))!= grid.end()){
-                    none = false;
-                    break;
-                }
-            }
-            if(none){
-                return 3;
-            }
-            none = true;
-            break;
-        case 2:
-            for(int i=-1; i<=1; i++){
-                if(grid.find(std::make_pair(point.first + i,point.second - 1))!= grid.end()){
-                    none = false;
-                    break;
-                }
-            }
-            if(none){
-                return -4;
-            }
-            none = true;
-            break;
-        case 3:
-            for(int i=-1; i<=1; i++){
-                if(grid.find(std::make_pair(point.first + i,point.second + 1))!= grid.end()){
-                    none = false;
-                    break;
-                }
-            }
-            if(none){
-                return 4;
-            }
-            none = true;
-            break;
+            i++;
         }
     }
-    return 0;
-}
-bool available(const std::map<std::pair<int,int>,int>& grid, const std::pair<int,int> point){
-    /*if(grid.find(point) != grid.end()){
-        assert(grid.at(point) == 0);
-        return true;
-    }*/
-    int count = 0;
-    for(int i=-1; i<=1; i++){
-        for(int j=-1; j<=1; j++){
-            auto p = std::make_pair(point.first+i,point.second+j);
-            if(grid.find(p) != grid.end()){
-                int move_dir = grid.at(p);
-                if(move_dir % 2 == -i && move_dir % 3 == -j){
-                    count++;
+    void update(int round){
+        if(none_moved){
+            return;
+        }
+        none_moved = true;
+        for(const auto pair : grid){
+            grid.at(pair.first) = movement_direction(pair.first, round);
+        }
+        auto new_grid = std::unordered_map<std::pair<int,int>,int>();
+        for(const auto pair : grid){
+            if(pair.second == 0){
+                new_grid.insert(std::move(pair));
+            }else{
+                auto p = std::make_pair(pair.first.first+(pair.second %2),pair.first.second+(pair.second %3));
+                if(available(p)){
+                    new_grid.emplace(p,0);
+                    none_moved = false;
+                }else{
+                    new_grid.emplace(pair.first,0);
                 }
             }
         }
+        grid = new_grid;
     }
-    return count < 2;
-}
-std::map<std::pair<int,int>,int> update(std::map<std::pair<int,int>,int>& grid, int round){
-    bool none_moved = true;
-    for(const auto pair : grid){
-        if(has_neighbor(grid,pair.first)){
-            grid.at(pair.first) = movement_direction(grid,pair.first, round);
-        }
+    bool finished(){
+        return none_moved;
     }
-    auto new_grid = std::map<std::pair<int,int>,int>();
-    for(const auto pair : grid){
-        auto p = std::make_pair(pair.first.first+(pair.second %2),pair.first.second+(pair.second %3));
-        if(available(grid,p)){
-            new_grid.emplace(p,0);
-            if(p != pair.first){
-                none_moved = false;
+    int empty_squares() const{
+        int minx = INT_MAX;
+        int miny = INT_MAX;
+        int maxx = INT_MIN;
+        int maxy = INT_MIN;
+        for(const auto pair : grid){
+            if(pair.first.first < minx){
+                minx = pair.first.first;
             }
-        }else{
-            new_grid.emplace(pair.first,0);
-        }
-    }
-    if(none_moved){
-        std::cout<<"ON ROUND "<<round+1<<std::endl;
-        assert(false);
-    }
-    return new_grid;
-}
-
-int main(){
-    std::string line;
-    auto grid = std::map<std::pair<int,int>,int>();
-    int i=0;
-    while(std::getline(std::cin,line)){
-        if(line.back() == '\r'){line.pop_back();}
-        int j=0;
-        for(char c : line){
-            if(c == '#'){
-                grid.emplace(std::make_pair(i,j),0);
+            if(pair.first.first > maxx){
+                maxx = pair.first.first;
             }
-            j++;
+            if(pair.first.second < miny){
+                miny = pair.first.second;
+            }
+            if(pair.first.second > maxy){
+                maxy = pair.first.second;
+            }
         }
-        i++;
+        std::cout<<minx<<','<<maxx<<std::endl;
+        std::cout<<miny<<','<<maxy<<std::endl;
+        int total = (maxx - minx + 1)*(maxy - miny + 1);
+        return total - grid.size();
     }
-    //for(int i=0; i<10; i++){
-    for(int i=0; i< INT_MAX; i++){
-        /*std::cout<<i<<std::endl;
+    void print(){
         for(const auto pair : grid){
             std::cout<<pair.first.first<<','<<pair.first.second<<":  "<<pair.second<<std::endl;
         }
-        std::cout<<std::endl;*/
-        grid = update(grid, i);
+        std::cout<<std::endl;
     }
-    int minx = INT_MAX;
-    int miny = INT_MAX;
-    int maxx = INT_MIN;
-    int maxy = INT_MIN;
-    for(const auto pair : grid){
-        std::cout<<pair.first.first<<','<<pair.first.second<<":  "<<pair.second<<std::endl;
-        if(pair.first.first < minx){
-            minx = pair.first.first;
+    private:
+    std::unordered_map<std::pair<int,int>,int> grid;
+    bool none_moved;
+    int movement_direction(const std::pair<int,int> point, int round) const{
+        assert(grid.at(point) == 0);
+        auto no_elf = std::bitset<9>();
+        //std::cout<<"Checking neighbors of "<<point.first<<','<<point.second<<std::endl;
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                bool has_elf = grid.find(std::make_pair(point.first + i - 1,point.second + j - 1)) != grid.end();
+                //std::cout<<has_elf<<std::endl;
+                no_elf.set(3*i+j,!has_elf);
+            }
         }
-        if(pair.first.first > maxx){
-            maxx = pair.first.first;
+        no_elf.set(3*1+1,true);
+        if(no_elf.all()){
+            return 0;
         }
-        if(pair.first.second < miny){
-            miny = pair.first.second;
+        for(int k=0; k<4; k++){
+            switch((k+round)%4){
+                case 0:
+                    if(no_elf[0] && no_elf[1] && no_elf[2]){
+                        return -3;
+                    }
+                    break;
+                case 1:
+                    if(no_elf[6] && no_elf[7] && no_elf[8]){
+                        return 3;
+                    }
+                    break;
+                case 2:
+                    if(no_elf[0] && no_elf[3] && no_elf[6]){
+                        return -4;
+                    }
+                    break;
+                case 3:
+                    if(no_elf[2] && no_elf[5] && no_elf[8]){
+                        return 4;
+                    }
+                    break;
+            }
         }
-        if(pair.first.second > maxy){
-            maxy = pair.first.second;
-        }
+        return 0;
     }
-    std::cout<<minx<<','<<maxx<<std::endl;
-    std::cout<<miny<<','<<maxy<<std::endl;
-    int total = (maxx - minx + 1)*(maxy - miny + 1);
-    std::cout<<total-grid.size()<<std::endl;
+    bool available(const std::pair<int,int> point) const{
+        int count = 0;
+        auto p = std::make_pair(point.first+1,point.second);
+        if(grid.find(p) != grid.end()){
+            int move_dir = grid.at(p);
+            if(move_dir % 2 == -1 && move_dir % 3 == 0){
+                count++;
+            }
+        }
+        p = std::make_pair(point.first-1,point.second);
+        if(grid.find(p) != grid.end()){
+            int move_dir = grid.at(p);
+            if(move_dir % 2 == 1 && move_dir % 3 == 0){
+                count++;
+            }
+        }
+        p = std::make_pair(point.first,point.second+1);
+        if(grid.find(p) != grid.end()){
+            int move_dir = grid.at(p);
+            if(move_dir % 2 == 0 && move_dir % 3 == -1){
+                count++;
+            }
+        }
+        p = std::make_pair(point.first,point.second-1);
+        if(grid.find(p) != grid.end()){
+            int move_dir = grid.at(p);
+            if(move_dir % 2 == 0 && move_dir % 3 == 1){
+                count++;
+            }
+        }
+        return count < 2;
+    }
+};
+
+int main(){
+    std::string line;
+    auto lines = std::vector<std::string>();
+
+    while(std::getline(std::cin,line)){
+        if(line.back() == '\r'){line.pop_back();}
+        lines.emplace_back(line);
+    }
+    auto grid = Grid(lines);
+    for(int i=0; i<10; i++){
+        grid.update(i);
+    }
+    std::cout<<grid.empty_squares()<<std::endl;
+    int i=10;
+    while(!grid.finished()){
+        grid.update(i);
+        i++;
+    }
+    std::cout<<"FINISHED ON ROUND "<<i<<std::endl;
 }
